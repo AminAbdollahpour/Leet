eab74b987ffad2b1af4fc47a68550e40701b3a72
Amin Abdollahpour <mohammadaminabdolahpour@gamil.com>
2023-07-11 00:25:24 +0430

    page_item_count needs to be modified.

--- kyu7/PaginationHelper.py
+++ kyu7/PaginationHelper.py
@@ -20,7 +20,13 @@ class PaginationHelper:
     # returns the number of items on the given page. page_index is zero based
     # this method should return -1 for page_index values that are out of range
     def page_item_count(self, page_index):
-        pass
+        if self.item_count() > self.page_count():
+            if page_index + 1 >= self.page_count() * 2:
+                return 2
+            elif page_index + 1 >= self.page_count():
+                return 1
+            elif page_index + 1 <= self.page_count():
+                return 0
 
     # determines what page an item at the given index is on. Zero based indexes.
     # this method should return -1 for item_index values that are out of range
