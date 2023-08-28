8b6ba55bc34022d69f5cd2e49348024879b10062
Amin Abdollahpour <mohammadaminabdolahpour@gmail.com>
2023-08-28 16:59:41 +0430

    10th

---
+++ 75LeetCodeProblems/LongestIncreasingSubsequence.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def length_of_list(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+        for i in range(len(nums)-1,-1,-1):
+            for j in range(i+1,len(nums)):
+                if nums[i]<nums[j]:
+                    LIS[i] = max(LIS[i] , 1+LIS[j])
+        return max(LIS)
