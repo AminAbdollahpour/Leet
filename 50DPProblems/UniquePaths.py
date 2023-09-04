fc9e9eb14b8d4e37b68f6efdd230881045ecec1b
asus <mohammadaminabdolahpour@gmail.com>
2023-09-04 20:12:42 +0430

    7th

---
+++ 50DPProblems/UniquePaths.py
@@ -0,0 +1,13 @@
+import math
+
+
+class Solution:
+    def unique_paths(self, m: int, n: int) -> int:
+        top = (m - 1) + (n - 1)
+        minNum = min(m - 1, n - 1)
+        down = math.factorial(minNum) * math.factorial(top - minNum)
+        return int(math.factorial(top) / down)
+
+
+s = Solution()
+print(s.unique_paths(3, 7))
