cfc90cf9591800619661983b10005d12bbdeaf43
asus <mohammadaminabdolahpour@gmail.com>
2023-09-07 11:26:40 +0430

    10th

---
+++ 75LeetCodeProblems/605. Can Place Flowers.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        lenF = len(flowerbed)
+        flowerbed.append(0)
+        flowerbed.insert(0, 0)
+        temp = 0
+        for i in range(1, lenF + 1):
+            if flowerbed[i] != 1 and flowerbed[i + 1] != 1 and flowerbed[i - 1] != 1:
+                temp += 1
+                flowerbed[i] = 1
+        if temp >= n:
+            return True
+        return False
+
+
+s = Solution()
+print(s.canPlaceFlowers(flowerbed=[1, 0, 0, 0, 1, 0, 0], n=2))
