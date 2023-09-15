51fa264cfadff928063a365e6a301ee783baee49
asus <mohammadaminabdolahpour@gmail.com>
2023-09-15 16:40:35 +0430

    17th

--- 50DPProblems/139. Word Break.py
+++ 50DPProblems/139. Word Break.py
@@ -3,16 +3,16 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        lens = []
-        for word in wordDict:
-            lens.append(len(word))
-        for word in wordDict:
-            if len(s) < min(lens):
-                return False
-            elif word == s[:len(word)]:
-                return self.wordBreak(s[:len(word)], wordDict)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        max_len = max(map(len, wordDict))
 
+        for i in range(1, n + 1):
+            for j in range(i - 1, max(i - max_len - 1, -1), -1):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
 
+        return dp[n]
 
-s = Solution()
-print(s.wordBreak(s="leetcode", wordDict=["leet", "code"]))
