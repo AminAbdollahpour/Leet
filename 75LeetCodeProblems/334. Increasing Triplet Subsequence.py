3f4a56379a42797ca60b3e24332c05e4519278d8
asus <mohammadaminabdolahpour@gmail.com>
2023-09-19 23:21:43 +0430

    13th

---
+++ 75LeetCodeProblems/334. Increasing Triplet Subsequence.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first , second = inf , inf
+        for third in nums:
+            if second < third:
+                return True
+            if third <= first:
+                first = third
+            else:
+                second = third
+        return False
