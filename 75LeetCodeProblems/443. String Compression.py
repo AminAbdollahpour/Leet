529b1f28277376c9cc05f71bbd1adfbc1b1a2ec9
asus <mohammadaminabdolahpour@gmail.com>
2023-09-26 22:47:08 +0330

    updated

--- 75LeetCodeProblems/443. String Compression.py
+++ 75LeetCodeProblems/443. String Compression.py
@@ -1,17 +1,20 @@
 from typing import List
+
+
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        s = []
-        for i in range(len(chars)):
-            if chars[i] in s:
-                continue
-            else:
-                s.append(chars[i])
-                if chars.count(chars[i])>10:
-                    s.append(str(chars.count(chars[i]))[0])
-                    s.append(str(chars.count(chars[i]))[1])
-                elif chars.count(chars[i])>1:
-                    s.append(str(chars.count(chars[i])))
-
-        return len(s)
-
+        i = 0
+        res = 0
+        while i < len(chars):
+            group_length = 1
+            while (i + group_length < len(chars)
+                   and chars[i + group_length] == chars[i]):
+                group_length += 1
+            chars[res] = chars[i]
+            res += 1
+            if group_length > 1:
+                str_repr = str(group_length)
+                chars[res:res + len(str_repr)] = list(str_repr)
+                res += len(str_repr)
+            i += group_length
+        return res
