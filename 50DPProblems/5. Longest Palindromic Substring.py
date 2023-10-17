6a4254aff1bce23235ddfd7963effdbbfca003c3
asus <mohammadaminabdolahpour@gmail.com>
2023-10-17 12:16:08 +0330

    new code

--- 50DPProblems/5. Longest Palindromic Substring.py
+++ 50DPProblems/5. Longest Palindromic Substring.py
@@ -1,17 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        left = 0
-        right = 1
-        longest_palindromic = 0
-        result = ''
-        while right > left:
-            clone = s[left:right + 1]
-            if s[left:right + 1] == clone[::-1]:
-                if len(clone) > longest_palindromic:
-                    longest_palindromic = len(clone)
-                    result = clone
-                    left = right
-                    right += 1
-                else:
-                    right += 1
+        def expand(l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l+1:r]
+
+        result = ""
+        for i in range(len(s)):
+            sub1 = expand(i, i)
+            if len(sub1) > len(result):
+                result = sub1
+            sub2 = expand(i, i+1)
+            if len(sub2) > len(result):
+                result = sub2
         return result
