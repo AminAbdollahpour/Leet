08411966f9ed294350d9c70ec10be1310639296b
asus <mohammadaminabdolahpour@gmail.com>
2023-10-18 13:45:36 +0330

    49

---
+++ LeetCodeProblems/49. Group Anagrams.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+
+        return list(anagram_map.values())
+
