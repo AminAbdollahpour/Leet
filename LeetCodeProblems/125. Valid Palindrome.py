b8ee8e037cfd86946dd7599b56f102d1b7a0bd03
asus <mohammadaminabdolahpour@gmail.com>
2023-10-23 17:34:22 +0330

    O(1) MC solution

--- LeetCodeProblems/125. Valid Palindrome.py
+++ LeetCodeProblems/125. Valid Palindrome.py
@@ -3,11 +3,19 @@ import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newS = re.sub(r'\W+', '', s).lower().replace('_','')
-        print(newS)
-        if newS == newS[::-1]:
-            return True
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while l < r and not self.alphaNum(s[r]):
+                r -= 1
 
-        return False
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l + 1, r - 1
+        return True
 
-
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or
+                ord('0') <= ord(c) <= ord('9'))
