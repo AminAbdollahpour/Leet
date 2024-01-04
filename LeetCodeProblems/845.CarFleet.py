b80a53ad461dce7e1b5ad25a5b40a485d8401d08
asus <mohammadaminabdolahpour@gmail.com>
2024-01-05 00:27:48 +0330

    leetcode 845

--- LeetCodeProblems/845.CarFleet.py
+++ LeetCodeProblems/845.CarFleet.py
@@ -3,15 +3,13 @@ from typing import List
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        time_to_destination = []
-        n = len(position)
-        for i in range(n):
-            time_to_destination.append((target - position[i]) / speed[i])
-        for j in range(n):
-            for k in range(n):
-                if (position[j] < position[k]) and (time_to_destination[j] < time_to_destination[k]):
-                    time_to_destination[j] = time_to_destination[k]
-        return len(set(time_to_destination))
+        pair = [[p, s] for p, s in zip(position, speed)]
+        stack = []
+        for p, s in sorted(pair)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
 
 
 s = Solution()
