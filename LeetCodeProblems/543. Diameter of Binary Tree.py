2ddc3db1a3b62402d1889cf528339a863bf5717e
asus <mohammadaminabdolahpour@gmail.com>
2024-01-13 22:39:06 +0330

    leetcode 543

--- LeetCodeProblems/543. Diameter of Binary Tree.py
+++ LeetCodeProblems/543. Diameter of Binary Tree.py
@@ -11,19 +11,15 @@ class TreeNode:
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        ans = [0]
 
-        def getDepth(node, depth):
-            if node is None:
-                return depth
+        def dfs(root):
+            if not root:
+                return -1
+            left = dfs(root.left)
+            right = dfs(root.right)
+            ans[0] = max(ans[0], 2 + left + right)
+            return 1 + max(left, right)
 
-            leftDepth = getDepth(node.left, depth + 1)
-            rightDepth = getDepth(node.right, depth + 1)
-            return max(leftDepth, rightDepth)
-
-        right = getDepth(root.right, 0)
-        left = getDepth(root.left, 0)
-        diameter = max(right + left, self.diameterOfBinaryTree(root.left), self.diameterOfBinaryTree(root.right))
-
-        return diameter
+        dfs(root)
+        return ans[0]
