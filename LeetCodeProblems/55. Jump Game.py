07e5d7467550d3203cdbdd37226586b0230ea8a5
asus <mohammadaminabdolahpour@gmail.com>
2024-03-17 16:50:30 +0330

    leetcode 55

--- LeetCodeProblems/55. Jump Game.py
+++ LeetCodeProblems/55. Jump Game.py
@@ -3,19 +3,9 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
         goal = len(nums) - 1
         for i in reversed(range(len(nums) - 1)):
-            print(i)
-            print(goal)
             if i + nums[i] >= goal:
-                if i == 0:
-                    return True
                 goal = i
 
-        return False
-
-
-s = Solution()
-print(s.canJump(nums=[1]))
+        return True if goal == 0 else False
