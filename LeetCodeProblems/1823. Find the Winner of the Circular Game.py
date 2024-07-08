7e46dbef9a6330ecdad528c24f89d07528afb51b
asus <mohammadaminabdolahpour@gmail.com>
2024-07-08 19:28:13 +0330

    day 2

---
+++ LeetCodeProblems/1823. Find the Winner of the Circular Game.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        q = deque()
+        for i in range(1, n + 1):
+            q.append(i)
+
+        while len(q) > 1:
+            for i in range(k - 1):
+                q.append(q.popleft())
+            q.popleft()
+        return q[0]
+
+
+s = Solution()
+
+print(s.findTheWinner(n=5, k=2))
