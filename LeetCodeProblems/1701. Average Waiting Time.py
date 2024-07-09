9a8ff2644946710fcb86ee061d20007a38f33c26
asus <mohammadaminabdolahpour@gmail.com>
2024-07-09 16:23:46 +0330

    day 3

--- LeetCodeProblems/1701. Average Waiting Time.py
+++ LeetCodeProblems/1701. Average Waiting Time.py
@@ -3,15 +3,12 @@ from typing import List
 
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
-        right_now = customers[0][0]
+        next_idle_time = 0
         total_wait = 0
+
         for customer in customers:
-            if customer[0] < right_now:
-                additional_wait = abs(customer[0] - right_now)
-                total_wait += customer[1] + additional_wait
-            else:
-                total_wait += customer[1]
-            right_now += customer[1]
+            next_idle_time = max(customer[0], next_idle_time) + customer[1]
+            total_wait += next_idle_time - customer[0]
 
         return total_wait / len(customers)
 
