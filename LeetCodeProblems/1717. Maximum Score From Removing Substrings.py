4be41dc21de6e6361dce7931ea91fd4ec6730e16
asus <mohammadaminabdolahpour@gmail.com>
2024-07-12 17:56:47 +0330

    day 6 second attempt

--- LeetCodeProblems/1717. Maximum Score From Removing Substrings.py
+++ LeetCodeProblems/1717. Maximum Score From Removing Substrings.py
@@ -2,32 +2,24 @@ class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
         arr = list(s)
         ans = 0
-        i = 0
-        j = 0
-        yba = 'ba'
-        xab = 'ab'
-        if x > y:
-            y, x = x, y
-            yba, xab = xab, yba
-        while i < len(arr):
-            portion = arr[i:i + 2]
-            if ''.join(portion) == yba:
-                print('wut')
-                del arr[i + 1], arr[i]
-                ans += y
-                i = -1
-            i += 1
-        print(arr)
-        print(ans)
-        while j < len(arr):
-            portion = arr[j:j + 2]
-            if ''.join(portion) == xab:
-                print('rot')
-                del arr[j + 1], arr[j]
-                ans += x
-                j = -1
-            j += 1
-        return ans
+
+        def remover(arr, find, y, ans):
+            i = 0
+            while i < len(arr):
+                portion = arr[i:i + 2]
+                if ''.join(portion) == find:
+                    del arr[i + 1], arr[i]
+                    ans += y
+                    i = -1
+                i += 1
+            return ans, arr
+        if x >= y:
+            first = remover(arr, 'ab', x, ans)
+            ans = remover(first[1], 'ba', y, first[0])
+        else:
+            second = remover(arr, 'ba', y, ans)
+            ans = remover(second[1], 'ab', x, second[0])
+        return ans[0]
 
 
 s = Solution()
