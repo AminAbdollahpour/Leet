8f3ad12a4c99516afc3b5719ef65789f70cfc82a
Amin <mohammadaminabdolahpour@gmail.com>
2024-07-15 01:53:30 +0330

    Delete LeetGo directory

--- LeetGo/1550. Three Consecutive Odds.go
+++
@@ -1,9 +0,0 @@
-func threeConsecutiveOdds(arr []int) bool {
-	for i := 0; i < len(arr)-2; i++ {
-		if arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 != 0 {
-			return true
-
-		}
-	}
-	return false
-}
