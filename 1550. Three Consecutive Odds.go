94170ab60631c4883fbf128d876e7a7b6b1147ad
Amin <mohammadaminabdolahpour@gmail.com>
2024-07-15 01:33:32 +0330

    Create 1550. Three Consecutive Odds.go

---
+++ 1550. Three Consecutive Odds.go
@@ -0,0 +1,9 @@
+func threeConsecutiveOdds(arr []int) bool {
+	for i := 0; i < len(arr)-2; i++ {
+		if arr[i]%2 != 0 && arr[i+1]%2 != 0 && arr[i+2]%2 != 0 {
+			return true
+
+		}
+	}
+	return false
+}
