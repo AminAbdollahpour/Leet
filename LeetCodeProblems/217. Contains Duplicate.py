bc5caf3269ff2b141688d63e8dd13508a4fdd370
asus <mohammadaminabdolahpour@gmail.com>
2024-08-08 15:55:39 +0330

    resolved

--- LeetCodeProblems/217. Contains Duplicate.py
+++ LeetCodeProblems/217. Contains Duplicate.py
@@ -3,10 +3,7 @@ from typing import List
 
 class Solution:
     def contains_duplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        for index in range(len(nums) - 1):
-            if index == len(nums):
-                return False
-            elif nums[index] == nums[index + 1]:
-                return True
-        return False
+        setlist = set(nums)
+        if len(nums) == len(setlist):
+            return False
+        return True
